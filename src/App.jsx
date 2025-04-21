091951d29ae8524de0d03922a78ec23457d289e4
Kendaka <barengkendrick4@gmail.com>
2025-04-21 21:21:58 +0400

    added the tralalero tralala image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import "./App.css"
+import TralaleroMovement from './components/TralaleroMovement'
 
 function App() {
   return(
     <div className='bg-amber-950 h-screen w-full'>
-      <p className='text-white'>Hello!</p>
+      <TralaleroMovement />
     </div>
   )
 }
---
+++ src/components/TralaleroMovement.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import TralaleroImage from '../assets/tralalero.png';
+
+const TralaleroMovement = () => {
+    return(
+        <div>
+            <img 
+                src={TralaleroImage}
+                alt='Tralalero Movement'
+                className='w-24 h-auto'
+            />
+        </div>
+    )
+}
+
+export default TralaleroMovement;
