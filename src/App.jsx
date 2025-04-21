f15f52e2f99e5d3f50e3ea5b2a7da70e18497138
Kendaka <barengkendrick4@gmail.com>
2025-04-21 21:18:11 +0400

    added temporary bacground color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import "./App.css"
 
 function App() {
   return(
-    <div>
-      <p>Hello!</p>
+    <div className='bg-amber-950 h-screen w-full'>
+      <p className='text-white'>Hello!</p>
     </div>
   )
 }
