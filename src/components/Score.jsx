043c1027938aa67b5990232160759482b87a0ae2
Kendaka <barengkendrick4@gmail.com>
2025-04-23 15:34:37 +0400

    added a component

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Score = () => {
+    {/* High Score component */}
     return (
         <div className="absolute top-4 right-4 text-xl text-black font-bold bg-white/80 border border-gray-300 rounded-xl px-4 py-2 shadow-md">
             High Score: 0
@@ -9,4 +10,3 @@ const Score = () => {
 }
 
 export default Score;
-    
