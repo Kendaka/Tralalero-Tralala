6c4509f76586981cc598e22af4326784d919d5a1
Kendaka <barengkendrick4@gmail.com>
2025-04-24 11:15:41 +0400

    added a score that updates highscore as well

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Score = () => {
-    {/* High Score component */}
+const Score = ({ score = 0, highScore = 0, isGameActive = false }) => {
     return (
-        <div className="absolute top-4 right-4 text-xl text-black font-bold bg-white/80 border border-gray-300 rounded-xl px-4 py-2 shadow-md">
-            High Score: 0
-        </div>
+        <>
+            {/* Current Score (centered) */}
+            {isGameActive && (
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl text-white font-bold bg-black/50 rounded-xl px-6 py-3">
+                    {score}
+                </div>
+            )}
+            
+            {/* High Score (top right) */}
+            <div className="absolute top-4 right-4 text-xl text-black font-bold bg-white/80 border border-gray-300 rounded-xl px-4 py-2 shadow-md">
+                High Score: {highScore}
+            </div>
+        </>
     )
 }
 
